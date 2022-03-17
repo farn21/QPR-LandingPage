c8e75a9ec1843d8923f679257f3ac7a95230dcc9
unknown <desantiscarlos87@gmail.com>
2022-03-17 18:33:06 -0300

    LandingPage

---
+++ src/Components/QuePasa.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<main>
+    <h1>
+        ¿Que pasa,<br />
+        Riachuelo?
+    </h1>
+    <p>
+        QPR, permite organizar, sistematizar y compartir las visiones y el
+        conocimiento acumulado a lo largo de los años por organizaciones
+        sociales, científicos y otros actores que buscan soluciones para los
+        graves problemas socioambientales de la cuenca Matanza-Riachuelo.
+        Entendemos por justicia ambiental a las acciones que, a través de la
+        participación ciudadana en diversas instancias de discusión de políticas
+        públicas, apuntan a corregir los efectos de la distribución desigual de
+        recursos, en pos de garantizar el derecho al ambiente sano. Su objetivo
+        es minimizar y enmendar los grandes perjuicios que sufren quienes viven
+        en áreas expuestas a la contaminación -lo que pone en riesgo su salud- y
+        proteger los ecosistemas amenazados y sus servicios ambientales.
+    </p>
+    <button>Ingresa al mapa</button>
+</main>
+
+<style>
+</style>
